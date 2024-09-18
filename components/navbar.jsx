8a1c5314a070965cfb522a0ef34c65b63d8796af
Ryan <ryansheikh003@gmail.com>
2024-09-18 01:35:33 -0600

    fix: Resolve dark mode compatibility issues

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     };
 
     return (
-        <div className="flex border gap-5 p-2 justify-between ">
+        <div className="flex border-b border-accentborders gap-5 p-2 justify-between ">
             <div className=" hidden  sm:flex gap-2">
                 <Image src={'/logo.jpg'} width={50} height={50} className=" rounded-full" alt="logo" />
                 <div className=" flex flex-col">
@@ -30,11 +30,11 @@ const Navbar = () => {
                 <BsFilterLeft size={40} />
             </div>
             <div className=" flex  gap-5  h-full ">
-                <div className="h-1/2 gap-2 self-center rounded-xl border   flex justify-center items-center p-2">
+                <div className="h-1/2 gap-2 self-center rounded-xl border border-accentborders   flex justify-center items-center p-2">
                     <MdSearch size={25} />
-                    <input onChange={onChangeSearchInput} type="text" placeholder="Search any book" className="border-gray h-full w-full focus:outline-none"/>
+                    <input onChange={onChangeSearchInput} type="text" placeholder="Search any book" className=" bg-inputbg h-full w-full focus:outline-none"/>
                 </div>
-                <button className="h-1/2 flex rounded-lg p-2 items-center justify-center gap-2 bg-teal-500 text-white">
+                <button className="h-1/2 flex rounded-lg p-2 items-center justify-center gap-2 bg-accent text-white">
                     <CiSquarePlus size={25} />
                     <p className="hidden sm:inline"> Add new book </p>
                 </button>
